5966321becaf98b2cc7c749ee540087c00a58b29
Esduardo Del Aguila <daguilae@hotmail.com>
2020-03-27 09:31:03 -0600

    Script ayuda de GIT

---
+++ Scripts comandos GIT.sql
@@ -0,0 +1,19 @@
+/* 
+** Inicializar proyecto
+git init
+** Relacionar proyecto de Git con Github
+git remote add Laboratorio https://github.com/edelaguila/didactica.git
+** Actualizar repo de Git
+git pull Laboratorio Master
+** Creación de ramas
+git branch test
+git checkout test
+** Mezclando la rama con el MASTER
+git checkout master
+git merge test
+** Actualizar cambios en repo
+git add .
+git commit -m " mensaje "
+**  Actualizar cambios en GITHUB
+git push Laboratorio master
+git push Laboratorio test}
