712ed39e3e02fa75709022843b940f9634675075
Esduardo Del Aguila <daguilae@hotmail.com>
2020-04-21 12:58:05 -0600

    cambio de tamaño en clientes

--- FM/libreria.sql
+++ FM/libreria.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE empresarial2;
 use empresarial2;
+usuarios
 CREATE TABLE productos 
 (
 	codigoProductos VARCHAR(18) PRIMARY KEY NOT NULL,
-    nombreProductos VARCHAR(60),
+    nombreProductos VARCHAR(80),
     codigoMarca VARCHAR(5),
     codigoLinea VARCHAR(5),
     codigoTipoProducto VARCHAR(5),
@@ -11,6 +12,8 @@ CREATE TABLE productos
     estatusProductos VARCHAR(1),
     FOREIGN KEY (codigoMarca) REFERENCES marcas(codigoMarca),
     FOREIGN KEY (codigoLinea) REFERENCES lineas(codigoLinea),
-    
+    FOREIGN KEY (codigoTipoProuducto) REFERENCES tipoproducto(codigoTipoProducto),
+    FOREIGN KEY (codigoAutores) REFERENCES autores(codigoAutores)
+    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
     
     
